51829079efd1f06f3fef0b60590201ac1ed1df6f
lifeonthecode <mohammodjuwelislam@gmail.com>
2024-07-10 15:54:08 +0600

    secondery portfolio mobile menu added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 const current_year = document.getElementById('current_year');
 const year = new Date().getFullYear();
 current_year.textContent = year;
+
+
+const menu = document.getElementById('menu');
+const overlay = document.getElementById('overlay');
+
+document.getElementById('menu_btn').addEventListener('click', () => {
+    menu.classList.add('show');
+    overlay.classList.add('show');
+});
+
+document.getElementById('close_menu').addEventListener('click', () => {
+    menu.classList.remove('show');
+    overlay.classList.remove('show');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('show');
+    overlay.classList.remove('show');
+})
